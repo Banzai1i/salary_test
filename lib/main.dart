171ed2b43580d1428f371e52a274376515255aae
Banzaiii <apurba.stha1@gmail.com>
2023-03-12 11:08:49 +0545

    updated the pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Income Calculator',
+      title: 'Salary Tax Calculator',
       home: IncomeCalculator(),
     );
   }
